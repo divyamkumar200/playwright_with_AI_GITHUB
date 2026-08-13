0aed90826054066096c9a7df85d4d719a316970d
divyamkumar200 <divyamkumar200@gmail.com>
2026-08-13 21:47:08 +0530

    Remove Firefox and WebKit projects from config

--- playwright.config.js
+++ playwright.config.js
@@ -12,8 +12,6 @@ const config = {
   },
   projects: [
     { name: 'Chromium', use: { browserName: 'chromium' } },
-    { name: 'Firefox', use: { browserName: 'firefox' } },
-    { name: 'WebKit', use: { browserName: 'webkit' } },
-  ],
+     ],
 };
 module.exports = config;
